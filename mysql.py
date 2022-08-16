b538561d7de2c3f11c8274b6a2ba2e247f6d3af0
martinalee94 <martinalee0528@gmail.com>
2022-08-16 13:29:57 +0900

    네이버 크롤링 코드 기록

---
+++ mysql.py
@@ -0,0 +1,59 @@
+import pymysql
+
+from constant import MYSQL_SERVER_CONF as MSC
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+connect_url = rf"mysql+pymysql://{MSC['user']}:{MSC['password']}@{MSC['host']}:{MSC['port']}/{MSC['db']}?charset=utf8mb4"
+
+engine = create_engine(connect_url,
+        encoding='utf-8', pool_size=1024, 
+        max_overflow=512, pool_recycle=240,
+        pool_pre_ping=True
+        )
+
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class MysqlConnector:
+    def __init__(self, cursor_type='dict'):
+        self.host = MSC['host']
+        self.port = MSC['port']
+        self.user = MSC['user']
+        self.passwd = MSC['password']
+        self.db = MSC['db']
+        self.cursor_type = cursor_type
+        self.connector = None
+
+    def __enter__(self):
+        self.connector = pymysql.connect(
+            user    = self.user,
+            passwd  = self.passwd,
+            port = self.port,
+            host    = self.host,
+            db      = self.db,
+            charset = 'utf8'
+        )
+        if self.cursor_type == 'tuple':
+            self.cursor = self.connector.cursor()
+        else:
+            self.cursor = self.connector.cursor(pymysql.cursors.DictCursor)
+        
+        return self.cursor
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if traceback is None:
+            self.connector.commit()
+        else:
+            self.connector.rollback()
+        
+        self.cursor.close()
+        self.connector.close()
+
+
+
---
+++ naver.sh
@@ -0,0 +1,3 @@
+58 * * * * /Users/martina/git/naver_t3c/naver_t3c_crawl.py >> /Users/martina/git/naver_t3c/cron.log
+ # 매시 58분인 이유는 밤12시 날짜 바뀔때 고려(네이버카페 작성시간 표시가 달라짐 시간에서 연월일로)
+
