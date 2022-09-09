2ce57388bef790e67bb655aae182641fc87b637a
Martina Lee <martinalee0528@gmail.com>
2022-09-10 03:35:15 +0900

    chore: db config

---
+++ db/constant.py
@@ -0,0 +1,10 @@
+import os
+
+MYSQL_SERVER_CONF = {
+    "user": os.environ.get("user"),
+    "password": os.environ.get("password"),
+    "host": os.environ.get("host"),
+    "port": 3306,
+    "db": os.environ.get("db"),
+    "charset": "utf8mb4",
+}
